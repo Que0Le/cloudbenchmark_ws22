fdbf3d0d8304c1610884b95e0a1e9c59c04bf606
Que0Le <Que0Le>
2023-01-06 17:35:37 +0100

    .

--- clustering_request.js
+++ clustering_request.js
@@ -69,6 +69,8 @@ async function clear_appwrite() {
  * @param {int} number_of_request 
  */
 async function request_worker(COLLECTION_ID, session_id, worker_id, number_of_request) {
+    // await sleep_ms(100)
+
     let result_all_requests = []
 
     for (let i = 0; i < number_of_request; i++) {
@@ -90,13 +92,12 @@ async function request_worker(COLLECTION_ID, session_id, worker_id, number_of_re
             // console.log(result_all_requests)
             // console.log("## All chunks resolved")
             write_array_of_results_to_file(result_all_requests, filename, is_2d = false, is_append = true)
-            .then(r => {
-                console.log("## Done writing data to file filename=" + filename);
-                has_exported_data = true
-            })
-            
+                .then(r => {
+                    // console.log("-- Done writing data to file filename=" + filename);
+                    has_exported_data = true
+                })
         }
-        await sleep_ms(100)
+        await sleep_ms(10)
     }
 }
 
@@ -105,10 +106,11 @@ const session_id = "test_session"
 async function handle_master() {
     let COLLECTION_ID = await clear_appwrite()
 
-    console.log(`## Number of CPUs is ${totalCPUs}`);
-    console.log(`## Master ${process.pid} is running`);
+    console.log(`++ Number of CPUs is ${totalCPUs}`);
+    console.log(`++ Master ${process.pid} is running`);
     let current_task = 0
-    const MAX_NBR_TASK = 10
+    let task_th_done = 0
+    const MAX_NBR_TASK = 12
     const task_size = 500
     
     // Fork workers.
@@ -116,25 +118,33 @@ async function handle_master() {
     for (var i = 0; i < totalCPUs; i++) {
         let worker = cluster.fork();
         worker.on('message', function (msg) {
-            if (msg.task_id && current_task < MAX_NBR_TASK) {
+            if ("task_id" in msg) {
+                console.log(msg.task_id, task_th_done++)
                 let new_task_id = current_task++;
-                console.log(`## Master => ${msg.worker_id}: ok, new task_id=${new_task_id}`)
-                worker.send({COLLECTION_ID: COLLECTION_ID, task_id: new_task_id, task_size: task_size, worker_id: msg.worker_id})
+                if (new_task_id < MAX_NBR_TASK) {
+                    console.log(`++ Master => ${msg.worker_id}: ok, new task_id=${new_task_id}`)
+                    worker.send({
+                        "COLLECTION_ID": COLLECTION_ID, "task_id": new_task_id, 
+                        "task_size": task_size, "worker_id": msg.worker_id
+                    })
+                }
             }
         });
         workers.push(worker)
     }
-    console.log(`## Initiated ${totalCPUs} workers. Start assigning tasks ...`)
+    console.log(`++ Initiated ${totalCPUs} workers. Start assigning tasks ...`)
     
     for (let i=0; i<totalCPUs; i++) {
         // fs.createWriteStream(`log_client_${worker_id}_${session_id}.txt`); // clear old log files
-        workers[i].send({task_id: current_task++, task_size: task_size/(i+1)})
+        workers[i].send({"task_id": current_task++, "task_size": task_size})
     }
     
     while (true) {
-        if (current_task > MAX_NBR_TASK) {
-            console.log("## Sent req_stop_collecting_stat")
-            req_stop_collecting_stat(session_id).catch(e => { console.log({ error: e }) })
+        // console.log(task_th_done)
+        if (task_th_done == MAX_NBR_TASK) {
+            req_stop_collecting_stat(session_id)
+            .then(r => {console.log("## Sent req_stop_collecting_stat")})
+            .catch(e => { console.log({ error: e }) })
             break
         }
         await sleep_ms(100)
@@ -147,14 +157,16 @@ if (cluster.isMaster) {
 } else {
     console.log(`-- Worker ${cluster.worker.id} started.`)
     process.on("message", (msg) => {
-        console.log(process.pid)
-        if (msg.exit_now) {
+        // console.log(process.pid)
+        if ("exit_now" in msg) {
             // Exit  
-        } else if (msg.task_size) {
+        } else if ("task_size" in msg) {
             // console.log(cluster.worker.id)
             request_worker(msg.COLLECTION_ID, session_id, cluster.worker.id, msg.task_size)
-            console.log(`-- Worker ${cluster.worker.id} done task_id=${msg.task_id}, task_size=${msg.task_size}`)
-            process.send({ worker_id: cluster.worker.id, task_id: msg.task_id, task_size: msg.task_size });
+                .then(r => {
+                    console.log(`-- Worker ${cluster.worker.id} done task_id=${msg.task_id}, task_size=${msg.task_size}`)
+                    process.send({ worker_id: cluster.worker.id, task_id: msg.task_id, task_size: msg.task_size });
+                })
         }
     })
 
--- read_data.py
+++ read_data.py
@@ -1,5 +1,5 @@
 # Load up JSON Function
-import json
+import json, os, re
 import numpy as np
 # Open our JSON file and load it into python
 # input_file = open ("outputfile.txt")
@@ -10,20 +10,25 @@ import numpy as np
 
 session_id = "test_session"
 log_sut_path = "log_sut_" + session_id + ".txt"
-log_client_path = "log_client_" + session_id + ".txt"
+# log_client_path = "log_client_" + session_id + ".txt"
 
+log_client_paths = [f for f in os.listdir() if re.search(r'log_client_\d{1,2}_' + session_id + '.txt', f)]
 
 
 client_t3_t0 = []
 client_data = []
 
-with open(log_client_path) as log_client_f:
-    for line in log_client_f:
-        res = json.loads(line)
-        client_data.append(res)
-        client_t3_t0.append(res["t3"] - res["t0"])
+for log_client_path in log_client_paths:
+    with open(log_client_path) as log_client_f:
+        for line in log_client_f:
+            res = json.loads(line)
+            if res["t3"] > 0:
+                client_data.append(res)
+                client_t3_t0.append(res["t3"] - res["t0"])
 
 print(np.percentile(client_t3_t0, 95))
+print(np.percentile(client_t3_t0, 50))
+print(np.percentile(client_t3_t0, 20))
 print(np.std(client_t3_t0, ddof=1))
 
 # sorted_client_data = sorted(client_data, key=lambda k : k["value"]["t0"]) 
